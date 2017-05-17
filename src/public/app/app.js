e50a438d9a117d7426365ba24ffec7feab4cf991
Aviv Carmi <avivcarmis@gmail.com>
2017-05-17 05:18:01 +0300

    clean???

---
+++ src/public/app/app.js
@@ -0,0 +1,441 @@
+'use strict';
+
+angular
+    .module('BlurAdmin', [
+        'ngAnimate',
+        'ui.bootstrap',
+        'ui.sortable',
+        'ui.router',
+        'ngTouch',
+        'toastr',
+        'smart-table',
+        "xeditable",
+        'ui.slimscroll',
+        'ngJsTree',
+        'angular-progress-button-styles',
+
+        'BlurAdmin.theme'
+    ])
+    .config(amChartConfig)
+    .config(routesConfig)
+    .run(initialize)
+    .controller('MonitorCtrl', monitorCtrl);
+
+function routesConfig($stateProvider) {
+
+    $stateProvider
+        .state('monitor', {
+            url: '/monitor/:serverId',
+            title: 'Monitor',
+            templateUrl: 'app/monitor.html',
+            controller: 'MonitorCtrl'
+        });
+
+}
+
+/** @ngInject */
+function initialize($rootScope, baSidebarService, $state) {
+
+    $rootScope.servers = {
+        example: {
+            title: "Example Server",
+            url: "http://localhost:8080/metrics",
+            meters: [
+                {
+                    "title": "Total Count",
+                    "width": 12,
+                    "type": "CHART",
+                    "path": [
+                        "Endpoint",
+                        "GET /get_user_by_id",
+                        "Enter",
+                        "Total Count"
+                    ]
+                }
+            ]
+        }
+    };
+
+    baSidebarService.addStaticItem({
+        title: 'Example',
+        icon: 'ion-android-home',
+        stateRef: 'monitor/example'
+    });
+
+    baSidebarService.addStaticItem({
+        title: 'Add New Pages',
+        icon: 'ion-android-add-circle',
+        stateRef: 'new'
+    });
+
+    $state.go('monitor', {serverId: 'example'});
+}
+
+/** @ngInject */
+function monitorCtrl($scope, $stateParams, $http, $rootScope, $location, baConfig, layoutPaths) {
+
+    $scope.serverId = $stateParams.serverId;
+    if (!$scope.serverId) {
+        return $location.path('/');
+    }
+
+    $scope.server = $rootScope.servers[$scope.serverId];
+    if (!$scope.server) {
+        return $location.path('/');
+    }
+
+    $scope.data = [];
+
+    $scope.update = function () {
+        $http
+            .get($scope.server.url)
+            .then(
+                function (response) {
+                    if (response.data) {
+                        $scope.data.push({
+                            time: $scope.getTime(),
+                            data: response.data
+                        });
+                        $scope.draw();
+                    }
+                }, function (e) {
+                    var i = 1;
+                    // alert("Something went wrong");
+                }
+            );
+        setTimeout($scope.update, 5000);
+    };
+
+    $scope.getTime = function () {
+        var date = new Date();
+        var minutes = date.getMinutes();
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var seconds = date.getSeconds();
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        return minutes + ":" + seconds;
+    };
+
+    $scope.draw = function () {
+        for (var i = 0; i < $scope.server.meters.length; i++) {
+            var meter = $scope.server.meters[i];
+            var data = $scope.getDataByPath(meter.path);
+            switch (meter.type) {
+                case "CHART":
+                    $scope.drawChart(i, data);
+                    break;
+            }
+        }
+    };
+
+    $scope.getDataByPath = function (pathArray) {
+        var result = [];
+        for (var i = 0; i < $scope.data.length; i++) {
+            result.push({
+                time: $scope.data[i].time,
+                value: $scope.getEntryByPath(pathArray, $scope.data[i].data)
+            });
+        }
+        return result;
+    };
+
+    $scope.getEntryByPath = function (pathArray, sample) {
+        if (!sample || pathArray.length === 0) {
+            return sample;
+        }
+        for (var i = 0; i < sample.length; i++) {
+            var sampleEntry = sample[i];
+            if (sampleEntry.title && sampleEntry.title === pathArray[0]) {
+                return $scope.getEntryByPath(pathArray.slice(1), sampleEntry.value);
+            }
+        }
+    };
+
+    $scope.drawChart = function (index, data) {
+        var layoutColors = baConfig.colors;
+        var lineChart = AmCharts.makeChart("meter" + index, {
+            type: 'serial',
+            theme: 'blur',
+            color: layoutColors.defaultText,
+            marginTop: 0,
+            marginRight: 15,
+            dataProvider: data,
+            valueAxes: [
+                {
+                    axisAlpha: 0,
+                    position: 'left',
+                    gridAlpha: 0.5,
+                    gridColor: layoutColors.border
+                }
+            ],
+            graphs: [
+                {
+                    id: 'g1',
+                    balloonText: '[[value]]',
+                    bullet: 'round',
+                    bulletSize: 8,
+                    lineColor: layoutColors.danger,
+                    lineThickness: 1,
+                    negativeLineColor: layoutColors.warning,
+                    type: 'smoothedLine',
+                    valueField: 'value'
+                }
+            ],
+            chartScrollbar: {
+                graph: 'g1',
+                gridAlpha: 0,
+                color: layoutColors.defaultText,
+                scrollbarHeight: 55,
+                backgroundAlpha: 0,
+                selectedBackgroundAlpha: 0.05,
+                selectedBackgroundColor: layoutColors.defaultText,
+                graphFillAlpha: 0,
+                autoGridCount: true,
+                selectedGraphFillAlpha: 0,
+                graphLineAlpha: 0.2,
+                selectedGraphLineColor: layoutColors.defaultText,
+                selectedGraphLineAlpha: 1
+            },
+            chartCursor: {
+                categoryBalloonDateFormat: 'YYYY',
+                cursorAlpha: 0,
+                valueLineEnabled: true,
+                valueLineBalloonEnabled: true,
+                valueLineAlpha: 0.5,
+                fullWidth: true
+            },
+            dataDateFormat: 'YYYY',
+            categoryField: 'time',
+            categoryAxis: {
+                minPeriod: 'YYYY',
+                parseDates: true,
+                minorGridAlpha: 0.1,
+                minorGridEnabled: true,
+                gridAlpha: 0.5,
+                gridColor: layoutColors.border,
+            },
+            export: {
+                enabled: true
+            },
+            creditsPosition: 'bottom-right',
+            pathToImages: layoutPaths.images.amChart
+        });
+
+        lineChart.addListener('rendered', zoomChart);
+        if (lineChart.zoomChart) {
+            lineChart.zoomChart();
+        }
+
+        function zoomChart() {
+            lineChart.zoomToIndexes(Math.round(lineChart.dataProvider.length * 0.4), Math.round(lineChart.dataProvider.length * 0.55));
+        }
+    };
+
+    $scope.update();
+
+}
+
+
+function amChartConfig(baConfigProvider) {
+    var layoutColors = baConfigProvider.colors;
+    AmCharts.themes.blur = {
+
+        themeName: "blur",
+
+        AmChart: {
+            color: layoutColors.defaultText,
+            backgroundColor: "#FFFFFF"
+        },
+
+        AmCoordinateChart: {
+            colors: [layoutColors.primary, layoutColors.danger, layoutColors.warning, layoutColors.success, layoutColors.info, layoutColors.primaryDark, layoutColors.warningLight, layoutColors.successDark, layoutColors.successLight, layoutColors.primaryLight, layoutColors.warningDark]
+        },
+
+        AmStockChart: {
+            colors: [layoutColors.primary, layoutColors.danger, layoutColors.warning, layoutColors.success, layoutColors.info, layoutColors.primaryDark, layoutColors.warningLight, layoutColors.successDark, layoutColors.successLight, layoutColors.primaryLight, layoutColors.warningDark]
+        },
+
+        AmSlicedChart: {
+            colors: [layoutColors.primary, layoutColors.danger, layoutColors.warning, layoutColors.success, layoutColors.info, layoutColors.primaryDark, layoutColors.warningLight, layoutColors.successDark, layoutColors.successLight, layoutColors.primaryLight, layoutColors.warningDark],
+            labelTickColor: "#FFFFFF",
+            labelTickAlpha: 0.3
+        },
+
+        AmRectangularChart: {
+            zoomOutButtonColor: '#FFFFFF',
+            zoomOutButtonRollOverAlpha: 0.15,
+            zoomOutButtonImage: "lens.png"
+        },
+
+        AxisBase: {
+            axisColor: "#FFFFFF",
+            axisAlpha: 0.3,
+            gridAlpha: 0.1,
+            gridColor: "#FFFFFF"
+        },
+
+        ChartScrollbar: {
+            backgroundColor: "#FFFFFF",
+            backgroundAlpha: 0.12,
+            graphFillAlpha: 0.5,
+            graphLineAlpha: 0,
+            selectedBackgroundColor: "#FFFFFF",
+            selectedBackgroundAlpha: 0.4,
+            gridAlpha: 0.15
+        },
+
+        ChartCursor: {
+            cursorColor: layoutColors.primary,
+            color: "#FFFFFF",
+            cursorAlpha: 0.5
+        },
+
+        AmLegend: {
+            color: "#FFFFFF"
+        },
+
+        AmGraph: {
+            lineAlpha: 0.9
+        },
+        GaugeArrow: {
+            color: "#FFFFFF",
+            alpha: 0.8,
+            nailAlpha: 0,
+            innerRadius: "40%",
+            nailRadius: 15,
+            startWidth: 15,
+            borderAlpha: 0.8,
+            nailBorderAlpha: 0
+        },
+
+        GaugeAxis: {
+            tickColor: "#FFFFFF",
+            tickAlpha: 1,
+            tickLength: 15,
+            minorTickLength: 8,
+            axisThickness: 3,
+            axisColor: '#FFFFFF',
+            axisAlpha: 1,
+            bandAlpha: 0.8
+        },
+
+        TrendLine: {
+            lineColor: layoutColors.danger,
+            lineAlpha: 0.8
+        },
+
+        // ammap
+        AreasSettings: {
+            alpha: 0.8,
+            color: layoutColors.info,
+            colorSolid: layoutColors.primaryDark,
+            unlistedAreasAlpha: 0.4,
+            unlistedAreasColor: "#FFFFFF",
+            outlineColor: "#FFFFFF",
+            outlineAlpha: 0.5,
+            outlineThickness: 0.5,
+            rollOverColor: layoutColors.primary,
+            rollOverOutlineColor: "#FFFFFF",
+            selectedOutlineColor: "#FFFFFF",
+            selectedColor: "#f15135",
+            unlistedAreasOutlineColor: "#FFFFFF",
+            unlistedAreasOutlineAlpha: 0.5
+        },
+
+        LinesSettings: {
+            color: "#FFFFFF",
+            alpha: 0.8
+        },
+
+        ImagesSettings: {
+            alpha: 0.8,
+            labelColor: "#FFFFFF",
+            color: "#FFFFFF",
+            labelRollOverColor: layoutColors.primaryDark
+        },
+
+        ZoomControl: {
+            buttonFillAlpha: 0.8,
+            buttonIconColor: layoutColors.defaultText,
+            buttonRollOverColor: layoutColors.danger,
+            buttonFillColor: layoutColors.primaryDark,
+            buttonBorderColor: layoutColors.primaryDark,
+            buttonBorderAlpha: 0,
+            buttonCornerRadius: 0,
+            gridColor: "#FFFFFF",
+            gridBackgroundColor: "#FFFFFF",
+            buttonIconAlpha: 0.6,
+            gridAlpha: 0.6,
+            buttonSize: 20
+        },
+
+        SmallMap: {
+            mapColor: "#000000",
+            rectangleColor: layoutColors.danger,
+            backgroundColor: "#FFFFFF",
+            backgroundAlpha: 0.7,
+            borderThickness: 1,
+            borderAlpha: 0.8
+        },
+
+        // the defaults below are set using CSS syntax, you can use any existing css property
+        // if you don't use Stock chart, you can delete lines below
+        PeriodSelector: {
+            color: "#FFFFFF"
+        },
+
+        PeriodButton: {
+            color: "#FFFFFF",
+            background: "transparent",
+            opacity: 0.7,
+            border: "1px solid rgba(0, 0, 0, .3)",
+            MozBorderRadius: "5px",
+            borderRadius: "5px",
+            margin: "1px",
+            outline: "none",
+            boxSizing: "border-box"
+        },
+
+        PeriodButtonSelected: {
+            color: "#FFFFFF",
+            backgroundColor: "#b9cdf5",
+            border: "1px solid rgba(0, 0, 0, .3)",
+            MozBorderRadius: "5px",
+            borderRadius: "5px",
+            margin: "1px",
+            outline: "none",
+            opacity: 1,
+            boxSizing: "border-box"
+        },
+
+        PeriodInputField: {
+            color: "#FFFFFF",
+            background: "transparent",
+            border: "1px solid rgba(0, 0, 0, .3)",
+            outline: "none"
+        },
+
+        DataSetSelector: {
+            color: "#FFFFFF",
+            selectedBackgroundColor: "#b9cdf5",
+            rollOverBackgroundColor: "#a8b0e4"
+        },
+
+        DataSetCompareList: {
+            color: "#FFFFFF",
+            lineHeight: "100%",
+            boxSizing: "initial",
+            webkitBoxSizing: "initial",
+            border: "1px solid rgba(0, 0, 0, .3)"
+        },
+
+        DataSetSelect: {
+            border: "1px solid rgba(0, 0, 0, .3)",
+            outline: "none"
+        }
+
+    };
+}
